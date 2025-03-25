aa54f591ebaa6d1ac7e1ea95fd91353ea8dbbbef
HadjiRami <hadji.rami@tutanota.com>
2025-03-25 12:56:45 -0400

    Display name

---
+++ Function/main.js
@@ -0,0 +1,18 @@
+function PrintHiName(){
+    alert("hello Doctor Nadim!");
+}
+PrintHiName();
+
+function Sum(){
+    var x = 5 y = 10;
+    console.log(x + y);
+}
+Sum();
+function DisplayName(){
+let MyName = "Nadim";
+if(MyName == "Nadim"){
+    console.log("Hello" + MyName);
+}else{
+    console.log("No No");
+}
+}
