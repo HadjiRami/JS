03a65e519e0bc39ea07aab5f56936791a30051f2
HadjiRami <hadji.rami@tutanota.com>
2025-03-25 13:13:09 -0400

    Switch Case

---
+++ Switch-Case/main.js
@@ -0,0 +1,19 @@
+// switch case
+let season = prompt("wat is your fav season");
+switch(season){
+    case "winter" :
+        //code will be Display.
+        alert("winter is so cold");
+        break;
+
+    case "summer" :
+        alert("summer is hot");
+        break;
+    case "Spring" : 
+    alert("spring is very fine");
+    break;
+    
+    default :
+    alert("it is ok");
+    break;
+}
