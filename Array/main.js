a3c335299f9d33f7e34d418a158f1e2d07687c74
HadjiRami <hadji.rami@tutanota.com>
2025-03-25 15:17:06 -0400

    indexof

--- Array/main.js
+++ Array/main.js
@@ -41,3 +41,27 @@ let list = ["did", "nin", "him"];
 list.push("ram", "Dah", "Mah", "Dinamec");
 console.log(list);
 list.unshift("sss","ddd","mmm");
+
+//remove items from arry
+list.shift();
+console.log(list);
+
+list.pop();
+console.log(list);
+
+// array sort and revers
+list.sort();
+console.log("sort "+list);
+console.log(list);
+
+list.reverse();
+console.log("revers "+list);
+console.log(list);
+
+let y = ["did1", "nin1", "him1"];
+let z = ["cvd", "vvin", "hvsim"];
+let w = y + z;
+console.log(w);
+let w1 = y.concat(z);
+console.log(w1);
+console.log(typeof(w));
