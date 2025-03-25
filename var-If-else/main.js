3b6885aba755f985a36b3b50ca2e366fe00d0c7d
HadjiRami <hadji.rami@tutanota.com>
2025-03-25 12:31:42 -0400

    Using changes and conditional sentemces

---
+++ var-If-else/main.js
@@ -0,0 +1,20 @@
+var name = "Mona";
+var Price = 2500;
+console.log(Price);
+console.log(Name);
+var x = 10;
+var Y = 20;
+if (x == Y){
+    alert("is it equal");
+}else{
+    alert("Not Equal");
+}
+
+var a = 10, b = 20;
+if(a > b){
+    console.log("No");
+}else if (a < b){
+    console.log("yes");
+}else{
+    alert("Tray agane");
+}
